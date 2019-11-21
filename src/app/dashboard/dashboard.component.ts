0da00bd33c3d30af814a525b31c85c24421c1130
Saurav Mohan V <sauravmohan2@gmail.com>
2019-11-21 19:54:02 +0530

    Updates
    Front-End-Updates

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -92,9 +92,9 @@ export class DashboardComponent implements OnInit {
       /* ----------==========     Completed Tasks Chart initialization    ==========---------- */
 
       const dataCompletedTasksChart: any = {
-          labels: ['12p', '3p', '6p', '9p', '12p', '3a', '6a', '9a'],
+          labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
           series: [
-              [230, 750, 450, 300, 280, 240, 200, 190]
+            [10, 13, 5, 3, 27, 10, 50]
           ]
       };
 
@@ -103,7 +103,7 @@ export class DashboardComponent implements OnInit {
               tension: 0
           }),
           low: 0,
-          high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+          high: 70, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
           chartPadding: { top: 0, right: 0, bottom: 0, left: 0}
       }
 
